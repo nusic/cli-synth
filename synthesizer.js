5bb63a11e4abb41a3fd9f2f59a48275616ea01bf
Erik Broberg <erikbroberg90@gmail.com>
2017-05-06 01:46:54 +0200

    Extract synthesizer logic to separate file

---
+++ synthesizer.js
@@ -0,0 +1,50 @@
+const exec = require('child_process').exec;
+
+function Synthesizer(){
+	this.settings = {
+		octave: 0,
+		transposition: 0,
+		current_sound: 0,
+		duration: 0.4,
+		fadein: 0.03,
+		fadeout: 0.3,
+		sound: 'tri',
+	};
+
+	this.procs = [];
+}
+
+Synthesizer.prototype.SOUNDS = ['sin', 'tri', 'squ', 'pluck'];
+
+/**
+	Outputs the provided tone as sound with the current synth settings
+	@param tone object with a defined frequency to play
+*/
+Synthesizer.prototype.play = function(tone) {
+	const sound = Synthesizer.prototype.SOUNDS[this.settings.current_sound];
+	const playCmd = `play -nq synth ${sound} ${tone.frequency} ${sound} ${Math.pow(2,7/12)*tone.frequency} fade ${this.settings.fadein} ${this.settings.duration} ${this.settings.fadeout} reverb `;
+	//console.log(playCmd);
+	var subproc = exec(playCmd, (error, stdout, stderr) => {
+	  if (error) {
+	    return;
+	  }
+	  if(stdout) console.log(`stdout: ${stdout}`);
+	  if(stderr) console.log(`stderr: ${stderr}`);
+	});
+	this.procs.push(subproc);
+}
+
+Synthesizer.prototype.destruct = function() {
+	this.procs.forEach(proc => proc.kill());
+};
+
+Synthesizer.prototype.nextSound = function() {
+	var n = Synthesizer.prototype.SOUNDS.length;
+	this.settings.current_sound = (n + this.settings.current_sound + 1) % n;
+}
+
+Synthesizer.prototype.sound = function() {
+	return Synthesizer.prototype.SOUNDS[this.settings.current_sound];
+};
+
+module.exports = Synthesizer;
