27201a5d85206adf70d63137e3cecfc2b7ac01c5
Erik Broberg <erikbroberg90@gmail.com>
2017-05-05 09:24:41 +0200

    init commit

---
+++ index.js
@@ -0,0 +1,102 @@
+var keypress = require('keypress');
+const exec = require('child_process').exec;
+var Keyboard = require('./keyboard');
+
+var keyboard = new Keyboard('awsedftgyhujkolpöä', 9);
+//var keyboard = new Keyboard('q2w3er5t6y7ucfvgbnjmk,l.', 9);
+
+console.log(keyboard.toString());
+
+var procs = [];
+process.on('exit', () => {
+	procs.forEach(proc => proc.kill());
+})
+
+// make `process.stdin` begin emitting "keypress" events 
+keypress(process.stdin);
+
+
+ 
+// listen for the "keypress" event 
+process.stdin.on('keypress', function (ch, key) {
+  //console.log(ch);
+  
+  if (key && key.ctrl && key.name == 'c') {
+    process.stdin.pause();
+    //procs.map(proc => console.log(proc.spawnargs[2]));
+    process.exit(1);
+  }
+  else {
+  	myOnPress(ch, key);
+  }
+});
+ 
+process.stdin.setRawMode(true);
+process.stdin.resume();
+
+
+var SOUNDS = ['sin', 'tri', 'squ', 'pluck'];
+
+var settings = {
+	transposition: 0,
+	octave: 0,
+	current_sound: 0,
+}
+
+var tone = {
+	duration: 0.4,
+	fadein: 0.03,
+	fadeout: 0.3,
+	sound: 'tri',
+	frequency: 440,
+};
+
+function myOnPress(ch, physicalKey){
+	//console.log(ch, physicalKey);
+	const key = keyboard.key(ch);
+	if(key){
+		tone.frequency = Math.pow(2, settings.octave + settings.transposition / 12) * key.frequency;
+		//procs.forEach(proc => proc.kill());
+		//console.log(ch);
+		play(tone);
+		//console.log(key.name);
+		//console.log('play');
+	}
+	else if(ch === ' '){
+		settings.current_sound = (SOUNDS.length + settings.current_sound + 1) % SOUNDS.length;
+	}
+	else if(physicalKey) {
+		switch(physicalKey.name){
+			case 'up': settings.octave++; break;
+			case 'down': settings.octave--; break;
+			case 'left': settings.transposition--; break;
+			case 'right': settings.transposition++; break;
+		}
+	}
+	
+	else {
+		//console.log(ch, key);
+	}
+	console.log(keyboard.toString(ch));
+	//updateMonitor();
+}
+
+function updateMonitor(){
+	process.stdout.clearLine();  // clear current text
+	process.stdout.cursorTo(0);  // move cursor to beginning of line
+	process.stdout.write('sound:'+SOUNDS[settings.current_sound] + ' octave:'+settings.octave + ' transposition:' + settings.transposition);  // write text
+}
+
+function play(tone){
+	const playCmd = `play -nq synth ${SOUNDS[settings.current_sound]} ${tone.frequency} ${SOUNDS[settings.current_sound]} ${Math.pow(2,7/12)*tone.frequency} fade ${tone.fadein} ${tone.duration} ${tone.fadeout} reverb `;
+	//console.log(playCmd);
+	var subproc = exec(playCmd, (error, stdout, stderr) => {
+	  if (error) {
+	    return;
+	  }
+	  if(stdout) console.log(`stdout: ${stdout}`);
+	  if(stderr) console.log(`stderr: ${stderr}`);
+	});
+	procs.push(subproc);
+}
+
---
+++ keyboard.js
@@ -0,0 +1,110 @@
+const KEY_NAMES = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#'];
+const NO_KEY = ' ';
+
+function keyType(semiTonesFromA){
+	while (semiTonesFromA < 0) semiTonesFromA += 12;
+	while (semiTonesFromA > 11) semiTonesFromA -= 12;
+	switch(semiTonesFromA){
+		case 0: 
+		case 5: 
+		case 10: return 'middle white';
+		case 2: 
+		case 7: return 'left white';
+		case 3: 
+		case 8: return 'right white';
+		default: return 'black';
+	}
+}
+
+function Keyboard(keyboardChars, locationStandardA){
+	this.keyboardChars = keyboardChars;
+	this.locationStandardA = locationStandardA;
+	this.charToKeyMap = buildKeyFreqMap(keyboardChars, locationStandardA);
+}
+
+Keyboard.prototype.key = function(ch) {
+	return this.charToKeyMap[ch];
+};
+
+Keyboard.prototype.charIndexIsBlackKey = function(charIndex) {
+	return this.charIsBlackKey(this.keyboardChars[charIndex]);
+};
+
+Keyboard.prototype.charIsBlackKey = function(ch) {
+	var key = this.charToKeyMap[ch];
+	return key !== null && key.name.length === 2; // its a sharp -> black
+};
+
+Keyboard.prototype.toString = function(charPressed) {
+	var lines = ['','','','',''];
+	for (var i = 0; i < this.keyboardChars.length; i++) {
+		const semiTonesFromA = i - this.locationStandardA;
+		var ch = this.keyboardChars[i];
+		if(ch === NO_KEY){
+			ch = this.charIndexIsBlackKey(i%12) ? ' ' : '░';
+		}
+		else if (ch === charPressed){
+			ch = 'X';
+		}
+		var CH = ch.toUpperCase();
+
+		const type = keyType(semiTonesFromA);
+		switch(type){
+			case 'left white':
+			lines[0] += ' ░░ ';
+			lines[1] += ' ░░ ';
+			lines[2] += ' ░░ ';
+			lines[3] += '░░░ ';
+			lines[4] += '░'+CH+'░ ';
+			break;
+
+			case 'right white':
+			lines[0] += '░░ ';
+			lines[1] += '░░ ';
+			lines[2] += '░░ ';
+			lines[3] += '░░░';
+			lines[4] += '░'+CH+'░';
+			break;
+
+			case 'middle white':
+			lines[0] += ' ░ ';
+			lines[1] += ' ░ ';
+			lines[2] += ' ░ ';
+			lines[3] += '░░░';
+			lines[4] += '░'+CH+'░';
+			break;
+
+			case 'black':
+			lines[0] += ' ';
+			lines[1] += ' ';
+			lines[2] += CH;
+			lines[3] += ' ';
+			lines[4] += ' ';
+		}
+	}
+	return lines.join('\n');
+};
+
+function buildKeyFreqMap(keyboard, locationStandardA){
+	if(locationStandardA < 0 || 11 < locationStandardA){
+		throw new Error('location of standard A must be within keyboard. You can octavize later');
+	}
+	const freqStandardA = 440;
+	return keyboard.split('').reduce((keyMap, key, location) => {
+		if(key === ' ') return keyMap;
+		const semiToneDiff = location - locationStandardA;
+		const octaveDiff = semiToneDiff / 12;
+		const freq = freqStandardA * Math.pow(2, octaveDiff);
+		const keyIndex = (12 + semiToneDiff) % 12;
+		keyMap[key] = {
+			frequency: freq,
+			name: KEY_NAMES[keyIndex],
+			semiToneDiff: semiToneDiff,
+		}
+		return keyMap;
+	}, {});
+}
+
+
+
+module.exports = Keyboard;
